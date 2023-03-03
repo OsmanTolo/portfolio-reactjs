9006847d7d54fef8a5849d08eca9d65451c2cdf7
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-03-03 19:16:48 +0000

    Add header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Home";
+import Header from "./components/Header";
 import About from "./components/pages/About";
 import Projects from "./components/pages/Projects";
 import Skills from "./components/pages/Skills";
