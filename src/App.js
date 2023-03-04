d8b8d887b5332bbc7574fb9db2f0bdf570c3fa3a
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-03-04 18:18:35 +0000

    clean start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import Header from "./components/Header";
-import About from "./components/pages/About";
-import Projects from "./components/pages/Projects";
-import Skills from "./components/pages/Skills";
-import Contact from "./components/pages/Contact";
-import Resume from "./components/pages/Resume";
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <NavTabs />
-        <Routes></Routes>
-      </div>
-    </Router>
-  );
-};
-
-export default App;
+export default function App() {
+  return <div></div>;
+}
